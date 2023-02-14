43fae87e1fabf9251f5364e17bbdd3d03d7ca996
root <root@DESKTOP-47GGDH6.localdomain>
2023-02-14 02:18:18 -0300

    creating prisma moodels e migrations

---
+++ prisma/migrations/20230213220351_creating_inital_models/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE `Users` (
+    `user_id` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `user_email` VARCHAR(500) NOT NULL,
+    `user_password` VARCHAR(30) NOT NULL,
+
+    PRIMARY KEY (`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Urls` (
+    `url_hash` VARCHAR(191) NOT NULL,
+    `url_original` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `expires_at` DATETIME NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`url_hash`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Urls` ADD CONSTRAINT `Urls_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+--Creating expires_at trigger
+CREATE TRIGGER `urls_expires_at` BEFORE INSERT ON `Urls` FOR EACH ROW SET new.expires_at = DATE_ADD(now(),interval 2 month);
